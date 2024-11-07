f49a4c182deae41cff97dfa1f36ff301a26d66e6
Tabshilla <adongtm@gmail.com>
2024-11-08 01:11:27 +0300

    updating portfolio

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,6 +2,8 @@ import { useState, useEffect  } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import { ArrowRightCircle } from "react-bootstrap-icons";
 import headerImg from "../assets/img/header-img.svg";
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
 
 
 export const Banner = () => {
@@ -51,10 +53,15 @@ export const Banner = () => {
             <Container>
                 <Row className="align-items-center">
                     <Col xs={12} md={6} xl={7}>
-                        <span className="tagline">Welcome to my Portfolio</span>
-                        <h1>{'Hi I am Tabshilla '} <span className="wrap">{text}</span></h1>
-                        <p>I'm a passionate and dedicated software engineer with a proven track record of developing high-quality applications. I have worked with various technologies and frameworks, including Python,Django, React, Node.js, and MySQL.</p>
-                        <button onClick={() => console.log('connect')}>Let's Connect <ArrowRightCircle size={25}/></button>
+                        <TrackVisibility>
+                        {({ isVisible }) =>
+                            <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+                                <span className="tagline">Welcome to my Portfolio</span>
+                                <h1>{'Hi I am Tabshilla '} <span className="wrap">{text}</span></h1>
+                                <p>I'm a passionate and dedicated software engineer with a proven track record of developing high-quality applications. I have worked with various technologies and frameworks, including Python,Django, React, Node.js, and MySQL.</p>
+                                <button onClick={() => console.log('connect')}>Let's Connect <ArrowRightCircle size={25}/></button>
+                            </div>}
+                        </TrackVisibility>
                      </Col>
                      <Col xs={12} md={6} xl={5}></Col>
                      <img src={headerImg} alt="Header Img" />
